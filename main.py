8116b46685ca4f3be526b981dcc0bac90b444b2a
Christopher Wang <dev@christopher.wang>
2024-02-27 22:04:31 -0500

    Add simple postgres connection

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+import psycopg2
+
+conn = psycopg2.connect("dbname=postgres user=postgres password=postgres host=localhost")
 
 app = FastAPI()
 
@@ -6,3 +9,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/health")
+async def health():
+    cur = conn.cursor()
+    cur.execute("SELECT 1")
+    result = cur.fetchone()
+    return {"pg": result}
